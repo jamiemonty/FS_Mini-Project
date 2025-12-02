75533f3ae867f7b2a3f7f29f49dff6bd1ef1cca2
Jamie <G00419525@atu.ie>
2025-12-02 19:48:02 +0000

    Added changes to the layout

--- Week11Lab2/next/fullStackWeek11lab2/components/layout/HamMenuContent.js
+++ Week11Lab2/next/fullStackWeek11lab2/components/layout/HamMenuContent.js
@@ -1,40 +1,53 @@
-import classes from './HamMenuContent.module.css'
-import { useRouter } from 'next/router'
-import { useContext, useState } from 'react'
-import GlobalContext from "../../pages/store/globalContext"
+import classes from './HamMenuContent.module.css';
+import { useRouter } from 'next/router';
+import { useContext } from 'react';
+import GlobalContext from "../../pages/store/globalContext";
 
 export default function HamMenuContent(props) {
-    const globalCtx = useContext(GlobalContext)
-    const router = useRouter()
-    let [popupToggle, setPopupToggle] = useState(false)
+    const globalCtx = useContext(GlobalContext);
+    const router = useRouter();
 
+    // Hide menu if global state says so
     if (globalCtx.theGlobalObject.hideHamMenu) {
-        return null
+        return null;
     }
 
-    function clicked(webAddress) {
-        globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: true })
-        router.push(webAddress)
+    // Navigate to the selected address and close menu
+    function handleMenuClick(webAddress) {
+        globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: true });
+        router.push(webAddress);
     }
 
-    function closeMe() {
-        globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: true })
-        if (popupToggle == true) {
-            setPopupToggle(false)
-        } else {
-            setPopupToggle(true)
+    // Close menu when background is clicked
+    function handleBackgroundClick(e) {
+        // Prevent closing if clicking inside the menu
+        if (e.target === e.currentTarget) {
+            globalCtx.updateGlobals({ cmd: 'hideHamMenu', newVal: true });
         }
     }
 
-    let contentJsx = props.contents.map((item, index) => (  //  [{title: 'Meeting 1', webAddress: '/meet1'}, {title: 'Meeting 2', webAddress: '/meet2'}]
-        <div className={classes.menuItem} key={index} onClick={() => clicked(item.webAddress)} >{item.title} </div>
-    ))
+    // Render menu items
+    const contentJsx = props.contents.map((item, index) => (
+        <div
+            className={classes.menuItem}
+            key={index}
+            onClick={() => handleMenuClick(item.webAddress)}
+            tabIndex={0}
+            role="button"
+            aria-label={`Go to ${item.title}`}
+            onKeyPress={e => {
+                if (e.key === 'Enter' || e.key === ' ') handleMenuClick(item.webAddress);
+            }}
+        >
+            {item.title}
+        </div>
+    ));
 
     return (
-        <div className={classes.background} onClick={() => closeMe()} >
-            <div className={classes.mainContent} >
+        <div className={classes.background} onClick={handleBackgroundClick}>
+            <nav className={classes.mainContent}>
                 {contentJsx}
-            </div>
+            </nav>
         </div>
     );
 }
